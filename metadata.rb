eab60c18aeb07639ec072e6889ef02b59b9462b7
Logan Koester <logan@logankoester.com>
2015-02-24 20:28:21 +0000

    Bumps 1.1.2

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'logan@logankoester.com'
 license          'MIT'
 description      'An enhanced, drop-in replacement for MySQL.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.1.1'
+version          '1.1.2'
 depends          'database', '2.3.1'
