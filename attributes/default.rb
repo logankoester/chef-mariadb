349f349fdd148e91795eae1d9ec63efb2b516bd0
Logan Koester <logan@logankoester.com>
2015-02-25 00:03:09 +0000

    Adds configurable bind-address and skip_name_resolve options for [mysqld]

--- attributes/default.rb
+++ attributes/default.rb
@@ -11,3 +11,7 @@ default['mariadb']['remove_anonymous_users'] = true
 default['mariadb']['remove_test_database'] = true
 
 default['mariadb']['supervisor'] = false
+
+# http://www.percona.com/blog/2008/05/31/dns-achilles-heel-mysql-installation/
+default['mariadb']['skip_name_resolve'] = false
+default['mariadb']['bind-address'] = '127.0.0.1'
