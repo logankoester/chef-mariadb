8cb5b2074fa0a170a0f42c8ac34e87fdf9322606
Logan Koester <logan@logankoester.com>
2018-12-01 01:23:19 +0700

    Renames package, updates ruby-version, updates guard-foodcritic

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
-name             'mariadb'
+name             'arch_mariadb'
 maintainer       'Logan Koester'
 maintainer_email 'logan@logankoester.com'
 license          'MIT'
 description      'An enhanced, drop-in replacement for MySQL.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.2.5'
-depends          'database', '2.3.1'
+depends          'database'
